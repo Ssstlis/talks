80ab2c2452efc395dfde1398b3ff6a665c069bb7
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 18:37:07 +0300

    RT5

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -2,18 +2,14 @@ object MainTest extends App {
   import scala.concurrent.{Future, Await}
   import scala.concurrent.duration.Duration
   import scala.concurrent.ExecutionContext.Implicits.global
+  import java.util.concurrent.atomic.AtomicInteger
 
-  val compute1 = { println("1") ; 1 }
-  val compute2 = { println("2") ; 2 }
-
-  val fa = Future(compute1)
-  val fb = Future(compute2)
+  val counter = new AtomicInteger(0)
 
   Await.result(
     for {
-      a1 <- fa
-      a2 <- fa
-      b1 <- fb
-      b2 <- fb
-    } yield println(a1 + a2 + b1 + b2), Duration.Inf)
+      a1 <- Future(counter.get)
+      _  <- Future(counter.set(a1 + 1))
+      a2 <- Future(counter.get)
+    } yield println(a1 + a2), Duration.Inf)
 }
