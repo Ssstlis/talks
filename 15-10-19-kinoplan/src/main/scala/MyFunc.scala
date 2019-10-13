63179a86a6d78e31d3d562dbb7b6f459d597a69f
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-14 00:52:15 +0300

    Make andThen

--- 15-10-19-kinoplan/src/main/scala/MyFunc.scala
+++ 15-10-19-kinoplan/src/main/scala/MyFunc.scala
@@ -1,3 +1,7 @@
 case class MyFunc[F[_], A, B](run: A => F[B]) {
   def compose[C](g: MyFunc[F, C, A]): MyFunc[F, C, B] = ???
+
+  def andThen[C](g: MyFunc[F, B, C]): MyFunc[F, A, C] = {
+    g.compose(this)
+  }
 }
