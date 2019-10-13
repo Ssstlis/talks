10bee1459a8bab7e0c0d05fb6b9d7ac5580e8f63
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-14 00:01:33 +0300

    Rewrite compose

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,15 +1,6 @@
 object MainTest extends App {
-  type =>?[A, B] = A => Option[B]
-  def compose[A, B, C](g: B =>? C)(f: A =>? B): A =>? C = f(_).flatMap(g)
-  def compose[A, B, C](g: B => Future[C])(f: A => Future[B]): A =>? C = {
-    f(_).flatMap(g)
-  }
 
-  def compose[A, B, C](g: B => List[C])(f: A => List[B]): A => List[C] = {
-    f(_).flatMap(g)
-  }
-
-  def compose[A, B, C](g: B => Either[?, C])(f: A => Either[?, B]): A => Either[?, C] = {
-    f(_).flatMap(g)
-  }
+  def compose[A, B, C, F[_]](g: B => F[C])(f: A => F[B])(
+    implicit ins: MyFlatMap[F]
+  ): A => F[C] = v => ins.flatMap(f(v))(g)
 }
