edf7f5b96ed87688f63de6b230c6a0e4644fcd52
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 20:27:40 +0300

    PF composition 2

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,14 +1,5 @@
 object MainTest extends App {
-  val pf1: PartialFunction[Int, Int] = {
-    case int if int % 2 == 0 => int
-  }
-
-  val pf2: PartialFunction[Int, String] = {
-    case int if int == 4 => "Success"
-  }
-
-  val pfLifted1: Int => Option[Int] = pf1.lift
-  val pfLifted2: Int => Option[String] = pf2.lift
-  val pfLifted3: Int => Option[String] = pfLifted2 compose pfLifted1
-
+  val f: Int => Option[Int] = ???
+  val g: Int => Option[String] = ???
+  val composed: Int => Option[String] = f.apply(_).flatMap(g)
 }
