2c903a0f8c34f32a920e6649f7a2511dfdb16311
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 19:53:42 +0300

    PF1

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,10 +1,7 @@
 object MainTest extends App {
-  val a: Int => Boolean = _ % 2 == 0
-  val b: Boolean => String = b => s"?$b?"
-  val c: String => String = s => s"\\$s\\"
+  val pfOddInt: PartialFunction[Int, Int] = {
+    case int if int % 2 == 0 => int
+  }
 
-  val d: Int => String = c compose b compose a
-
-  println(d.apply(2))
-  println(d.apply(1))
+  println(pfOddInt.apply(3))
 }
