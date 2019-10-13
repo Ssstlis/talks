6c2a0ee36b6f75275e0d6e0a11835531f39b0839
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 19:41:52 +0300

    Composition1

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,13 +1,3 @@
-import cats.effect.concurrent.Ref
-import cats.effect.{ExitCode, IO, IOApp}
-import cats.syntax.semigroupal.catsSyntaxSemigroupal
-
-object MainTest extends IOApp {
-  def run(args: List[String]): IO[ExitCode] = {
-    for {
-      ref <- Ref[IO].of(0)
-      (a1, a2) <- ref.modify(a => (a + 1, a)).product(ref.get)
-      _   <- IO.delay(println(a1 + a2))
-    } yield ExitCode.Success
-  }
+object MainTest extends App {
+  val a: Int => Boolean = _ % 2 == 0
 }
