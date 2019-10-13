0efbdd3eab231a52b5678d2167d05d87bce91738
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 18:26:07 +0300

    RT2

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,6 +1,6 @@
 object MainTest extends App {
-  def compute1 = { println("1") ; 1 }
-  def compute2 = { println("2") ; 2 }
+  val compute1 = { println("1") ; 1 }
+  val compute2 = { println("2") ; 2 }
 
   compute1
   compute2
