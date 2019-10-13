e324114d6639e0a77f345e9903c250100eebd45e
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 20:50:57 +0300

    Boilerplate 2

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,7 +1,15 @@
 object MainTest extends App {
   type =>?[A, B] = A => Option[B]
   def compose[A, B, C](g: B =>? C)(f: A =>? B): A =>? C = f(_).flatMap(g)
+  def compose[A, B, C](g: B => Future[C])(f: A => Future[B]): A =>? C = {
+    f(_).flatMap(g)
+  }
+
   def compose[A, B, C](g: B => List[C])(f: A => List[B]): A => List[C] = {
-    f.apply(_).flatMap(g)
+    f(_).flatMap(g)
+  }
+
+  def compose[A, B, C](g: B => Either[?, C])(f: A => Either[?, B]): A => Either[?, C] = {
+    f(_).flatMap(g)
   }
 }
