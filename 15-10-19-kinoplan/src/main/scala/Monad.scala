c579c2ad363256eabd432bee58f4383eb8c7bfbe
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-14 01:49:38 +0300

    Rework instances

--- 15-10-19-kinoplan/src/main/scala/Monad.scala
+++ 15-10-19-kinoplan/src/main/scala/Monad.scala
@@ -13,15 +13,21 @@ trait Monad[F[_]] extends Applicative[F] {
 object Monad {
   implicit val optionMonad = new Monad[Option] {
     def flatMap[A, B] = a => f => a.flatMap(f)
+
+    def pure[A] = Some(_)
   }
 
   implicit val listMonad = new Monad[List] {
     def flatMap[A, B] = a => f => a.flatMap(f)
+
+    def pure[A] = List(_)
   }
 
   implicit def mapMonad[K] = new Monad[Map[K, ?]] {
     def flatMap[A, B] = a => f => a.flatMap { case (k, v) =>
       f(v).get(k).map(k -> _)
     }
+
+    def pure[A] = ???
   }
 }
