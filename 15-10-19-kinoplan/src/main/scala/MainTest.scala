af92bf975f965fd690e437e64c257f67bb9cb704
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 18:31:38 +0300

    RT3

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,8 +1,19 @@
 object MainTest extends App {
-  val compute1 = { println("1") ; 1 }
-  val compute2 = { println("2") ; 2 }
+  import scala.concurrent.{Future, Await}
+  import scala.concurrent.duration.Duration
+  import scala.concurrent.ExecutionContext.Implicits.global
 
-  compute1
-  compute2
-  compute1
+  def compute1 = { println("1") ; 1 }
+  def compute2 = { println("2") ; 2 }
+
+  def fa = Future(compute1)
+  def fb = Future(compute2)
+
+  Await.result(
+    for {
+      a1 <- fa
+      a2 <- fa
+      b1 <- fb
+      b2 <- fb
+    } yield println(a1 + a2 + b1 + b2), Duration.Inf)
 }
