94096212d80c8c4144535c16142ae9df94de7f86
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-14 00:15:16 +0300

    prev example with option

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -1,6 +1,11 @@
 object MainTest extends App {
-
   def compose[A, B, C, F[_]](g: B => F[C])(f: A => F[B])(
     implicit ins: MyFlatMap[F]
   ): A => F[C] = v => ins.flatMap(f(v))(g)
+
+  val f: Int => Option[Int] = ???
+  val g: Int => Option[String] = ???
+  val j: String => Option[List[Char]] = ???
+
+  val composed: Int => Option[List[Char]] = compose(j)(compose(g)(f))
 }
