710876faee4f5fb82dbc916a01131b0d76cf2af5
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 19:42:34 +0300

    Composition4

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -2,4 +2,9 @@ object MainTest extends App {
   val a: Int => Boolean = _ % 2 == 0
   val b: Boolean => String = b => s"?$b?"
   val c: String => String = s => s"\\$s\\"
+
+  val d: Int => String = c compose b compose a
+
+  println(d.apply(2))
+  println(d.apply(1))
 }
