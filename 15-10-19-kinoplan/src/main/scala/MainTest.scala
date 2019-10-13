66d83708a0c560bf35ccdfeb9f2b2a1759b989d1
Aristov Ivan <Apostrof1995@gmail.com>
2019-10-13 20:46:17 +0300

    HKF compose 4

--- 15-10-19-kinoplan/src/main/scala/MainTest.scala
+++ 15-10-19-kinoplan/src/main/scala/MainTest.scala
@@ -4,5 +4,13 @@ object MainTest extends App {
   trait C { val list: List[B] }
   trait D { val list: List[C] }
 
-  val extract: D => List[A] = ???
+  def compose[A, B, C](g: B => List[C])(f: A => List[B]): A => List[C] = {
+    a => f.apply(a).flatMap(g)
+  }
+
+  val d2c: D => List[C] = _.list
+  val c2b: C => List[B] = _.list
+  val b2a: B => List[A] = _.list
+
+  val extract: D => List[A] = compose(b2a)(compose(c2b)(d2c))
 }
